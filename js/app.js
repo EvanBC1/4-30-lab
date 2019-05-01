4e1f243bab59e37076a6201aab8c4106c7b9bb91
starfoxz13@gmail.com <starfoxz13@gmail.com>
2019-05-01 11:24:29 -0700

    Added Scoring

--- js/app.js
+++ js/app.js
@@ -1,46 +1,55 @@
 "use strict";
 
+var userName = prompt("What is your name?")
+var score = 0;
+
 var answerOne = prompt("Is Africa the second largest continent?").toLowerCase();
 if(answerOne === 'yes' || answerOne === 'y') {
-    alert('you are correct, Africa is the second largest continent');
+    alert('you are correct '+ userName +', Africa is the second largest continent');
     console.log('The user got the correct answer for #1');
+    score++; 
 } else {
-    alert('you are wrong, Africa is the second largest continent');
+    alert('you are incorrect '+ userName +', Africa is the second largest continent');
     console.log('The user got the wrong answer for #1');
 }
 
 var answerTwo = prompt("Is the Amazon the longest river").toLowerCase();
 if(answerTwo === 'no' || answerTwo === 'n') {
-    alert('you are correct, the Nile is the longest river');
+    alert('you are correct '+ userName +', the Nile is the longest river');
     console.log('The user got the correct answer for #2');
+    score++; 
 } else {
-    alert('you are wrong, the Nile is the longest river');
+    alert('you are incorrect '+ userName +', the Nile is the longest river');
     console.log('The user got the wrong answer for #2');
 }
 
 var answerThree = prompt("Is Mt Everest the tallest mountain?").toLowerCase();
 if(answerThree === 'yes' || answerThree === 'y') {
-    alert('you are correct, Mt Everest the tallest mountain');
+    alert('you are correct '+ userName +', Mt Everest the tallest mountain');
     console.log('The user got the correct answer for #3');
+    score++; 
 } else {
-    alert('you are wrong, Mt Everest the tallest mountain');
+    alert('you are incorrect '+ userName +', Mt Everest the tallest mountain');
     console.log('The user got the wrong answer for #3');
 }    
 
 var answerFour = prompt("Is the US the 3rd largest Country").toLowerCase();
 if(answerFour === 'no' || answerFour === 'n') {
-    alert('you are correct, the US is the 4th largest country');
-    console.log('The user got the correct answer for #4')
+    alert('you are correct '+ userName +', the US is the 4th largest country');
+    console.log('The user got the correct answer for #4');
+    score++; 
 } else {
-    alert('you are wrong, the Us is the 4th largest country');
+    alert('you are incorrect '+ userName +', the Us is the 4th largest country');
     console.log('The user got the wrong answer for #4');
 }
 
 var answerFive = prompt("Is the Atlantic the biggest ocean?").toLowerCase();
 if(answerFive === 'no' || answerFive === 'n') {
-    alert('you are correct, the Atlantic is the second largest Ocean');
+    alert('you are correct '+ userName +', the Atlantic is the second largest Ocean');
     console.log('The user got the correct answer for #5');
+    score++; 
 } else {
-    alert('you are wrong, the Atlantic is the second largest Ocean');
+    alert('you are incorrect '+ userName +', the Atlantic is the second largest Ocean');
     console.log('The user got the wrong answer for #5');
 }
+document.getElementById("score").innerHTML = score;
